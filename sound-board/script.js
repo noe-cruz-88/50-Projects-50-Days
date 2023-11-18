1551f1fd58066b838778e90b7b3dfa161b5f1272
Noe Cruz <educacion.mcon88@gmail.com>
2023-11-17 19:19:17 -0600

    feat: creation of sound board

---
+++ sound-board/script.js
@@ -0,0 +1,25 @@
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
+
+sounds.forEach((sound) => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+
+    btn.innerText = sound
+
+    btn.addEventListener('click', () => {
+        stopSongs()
+
+        document.getElementById(sound).play()
+    })
+
+    document.getElementById('buttons').appendChild(btn)
+})
+
+function stopSongs() {
+    sounds.forEach((sounds) => {
+        const song = document.getElementById(sounds)
+
+        song.pause()
+        song.currentTime = 0
+    })
+}
